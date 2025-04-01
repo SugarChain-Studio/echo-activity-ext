56205495ffa478e9f63699d0adf3ebf2fcf2999f
dynilath <dynilath@qq.com>
2025-04-01 21:58:46 +0800

    fix: 修正资源预载

--- src/resouce.js
+++ src/resouce.js
@@ -18,6 +18,7 @@ export class Path {
         item.href = Path.resolve(path);
         item.rel = "preload";
         item.as = "image";
+        item.crossOrigin = "anonymous";
         document.head.appendChild(item);
         item.onload = () => {
             document.head.removeChild(item);
