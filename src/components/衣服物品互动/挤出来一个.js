12dfd6f2d4afc0711c6ee89191cf80413409db40
dynilath <dynilath@qq.com>
2025-04-27 02:56:09 +0800

    feat: 增加挤出来一个动作

---
+++ src/components/衣服物品互动/挤出来一个.js
@@ -0,0 +1,96 @@
+import { ActivityManager } from "../../activityForward";
+import { i18nAction } from "../../messager";
+
+import { DynImageProviders } from "../../dynamicImage";
+import { Prereqs } from "../../Prereqs";
+
+const beedsDef = {
+    AnalBeads2: 0.5,
+    大号拉珠: 0.3,
+};
+
+let cooldown = true;
+
+/** @type {CustomActivity} */
+const activity = {
+    activity: {
+        Name: "挤出来球",
+        Prerequisite: [
+            "TargetZoneAccessible",
+            () => cooldown,
+            Prereqs.Acted.GroupIs("ItemButt", Object.keys(beedsDef)),
+        ],
+        MaxProgress: 90,
+        Target: [],
+        TargetSelf: ["ItemButt"],
+    },
+    mode: "SelfOnSelf",
+    run: (player, _, { ActivityGroup }) => {
+        const item = InventoryGet(player, ActivityGroup.Name);
+        if (!item?.Property?.InsertedBeads) return;
+        let basePossiblity = beedsDef[item.Asset.Name];
+        if (!basePossiblity) return;
+
+        const arousal = PreferenceGetArousalZone(player, ActivityGroup.Name);
+        if (arousal) {
+            // 开发越多越容易
+            basePossiblity *= 0.1 + arousal.Factor * 0.9;
+            if (arousal.Orgasm) {
+                basePossiblity *= 1.5;
+            }
+        }
+
+        cooldown = false;
+
+        setTimeout(() => {
+            const chance = Math.random();
+            if (chance < basePossiblity) {
+                if (item.Property.InsertedBeads > 1) {
+                    item.Property.InsertedBeads--;
+                    item.Property.TypeRecord = { typed: item.Property.InsertedBeads - 1 };
+                } else {
+                    const itemGroup = item.Asset.Group.Name;
+                    InventoryRemove(player, itemGroup);
+                }
+                CharacterRefresh(player);
+                ChatRoomCharacterItemUpdate(player);
+
+                i18nAction(
+                    {
+                        CN: "SourceCharacter成功将屁股里的AssetName挤出来其中一个球.",
+                        EN: "SourceCharacter successfully squeezes out one ball of AssetName from the butt.",
+                    },
+                    {
+                        source: player,
+                        destination: player,
+                        asset: item.Asset,
+                        activity: { name: "MasturbateItem", group: "ItemButt" },
+                    }
+                );
+            } else {
+                i18nAction(
+                    {
+                        CN: "SourceCharacter尝试将塞在屁股里的AssetName挤出来，但失败了.",
+                        EN: "SourceCharacter tries to squeeze out AssetName stuck in the butt, but failed.",
+                    },
+                    { source: player, destination: player, asset: item.Asset }
+                );
+            }
+            cooldown = true;
+        }, 5000);
+    },
+    useImage: DynImageProviders.itemOnActedGroup("ItemButt"),
+    item: (player) => InventoryGet(player, "ItemButt"),
+    label: {
+        CN: "挤出来一个球",
+        EN: "Squeeze out one ball",
+    },
+    dialog: {
+        CN: "SourceCharacter用力试图将塞在屁股里的ActivityAsset挤出来一个.",
+        EN: "SourceCharacter tries to squeeze out one ActivityAsset stuck in the butt.",
+    },
+};
+
+export default function () {
+    ActivityManager.addCustomActivity(activity);
+}
