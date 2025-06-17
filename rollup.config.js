2ca7665271b97004a0442476edb9dfa2aba81fc9
dynilath <dynilath@qq.com>
2025-06-17 22:02:41 +0800

    build: 在构建输出中添加授权banner

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,25 @@ const commonjs = require("@rollup/plugin-commonjs");
 const resolve = require("@rollup/plugin-node-resolve");
 const terser = require("@rollup/plugin-terser");
 const { defineConfig } = require("rollup");
+const fs = require("fs");
 
 const packageJSON = require(path.join(process.cwd(), "package.json"));
+
+const banner = (() => {
+    const repo = (() => {
+        let url = packageJSON.repository?.url;
+        if (!url) return "";
+
+        if (url.startsWith("git+")) {
+            url = url.slice(4);
+        }
+        if (url.endsWith(".git")) {
+            url = url.slice(0, -4);
+        }
+        return url;
+    })();
+    return `/*\n${fs.readFileSync("LICENSE")}\n\n${repo}\n@preserve\n*/`;
+})();
 
 /**
  * @param {ReturnType<parseEnv>} param0
@@ -17,6 +34,7 @@ function createTranslationConfig({ destDir, beta = false, debug = false }) {
             file: `${destDir}${beta ? "/beta" : ""}/translation.js`,
             format: "es",
             sourcemap: debug ? "inline" : true,
+            banner,
         },
         treeshake: true,
         plugins: [commonjs(), resolve({ browser: true }), ...(debug ? [] : [terser({ sourceMap: true })])],
@@ -25,5 +43,5 @@ function createTranslationConfig({ destDir, beta = false, debug = false }) {
 
 module.exports = async (cliArgs) => {
     const env = parseEnv(__dirname, cliArgs);
-    return [await createModRollupConfig({ env, packageJSON }), createTranslationConfig(env)];
+    return [await createModRollupConfig({ env, packageJSON, banner }), createTranslationConfig(env)];
 };
