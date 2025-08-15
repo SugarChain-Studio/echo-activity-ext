e29fbf0bec7cc70940f3d2a6dab0e7c0298dd954
dynilath <dynilath@qq.com>
2025-08-15 11:54:51 +0800

    feat: 增加剪刀活动的目标项

--- src/components/衣服物品互动/剪刀.js
+++ src/components/衣服物品互动/剪刀.js
@@ -6,36 +6,56 @@ import { ActivityExt } from "../../activityext";
 const groupMap = {
     ItemTorso: ["Cloth", "Cloth_笨笨蛋Luzi", "Cloth_笨笨笨蛋Luzi2", "Suit", "Suit_笨笨蛋Luzi"],
     ItemLegs: ["ClothLower", "ClothLower_笨笨蛋Luzi", "ClothLower_笨笨笨蛋Luzi2", "SuitLower", "SuitLower_笨笨蛋Luzi"],
-    ItemBreast: ["Bra", "ClothAccessory", "Corset"],
-    ItemVulvaPiercings: ["Panties"],
+    ItemBreast: [
+        "Bra",
+        "Bra_笨笨蛋Luzi",
+        "ClothAccessory",
+        "ClothAccessory_笨笨蛋Luzi",
+        "ClothAccessory_笨笨笨蛋Luzi2",
+        "Corset",
+    ],
+    ItemVulvaPiercings: ["Panties", "Panties_笨笨蛋Luzi"],
     ItemFeet: ["Socks", "SocksLeft", "SocksRight"],
-    ItemBoots: ["Shoes"],
+    ItemBoots: ["Shoes", "Shoes_笨笨蛋Luzi"],
+    ItemHead: ["Hat", "Hat_笨笨蛋Luzi"],
+    ItemMouth: ["Mask", "Mask_笨笨蛋Luzi", "Mask_笨笨笨蛋Luzi2"],
+    ItemArms: ["Gloves", "Gloves_笨笨蛋Luzi"],
 };
 
-/** @type { CustomActivity } */
+/** @type { CustomActivity[] } */
 const activity = ActivityExt.fromTemplateActivity(
-    {
-        activity: {
-            Name: "剪刀剪掉上衣",
-            Prerequisite: ["UseHands", "UseArms", Prereqs.Acting.GroupIs("ItemHandheld", "Scissors")],
-            MaxProgress: 50,
-            Target: ["ItemTorso", "ItemLegs", "ItemBreast", "ItemVulvaPiercings", "ItemFeet", "ItemBoots"],
-            TargetSelf: true,
-        },
-        useImage: ["ItemHandheld", "Scissors"],
-        mode: "AnyOnSelf",
-        run: (player, sender, info) => {
-            // 遵守物品权限
-            if (!ServerChatRoomGetAllowItem(sender, player)) return;
+    [
+        {
+            activity: {
+                Name: "剪刀剪掉上衣",
+                Prerequisite: [
+                    "UseHands",
+                    "UseArms",
+                    Prereqs.Acting.GroupIs("ItemHandheld", "Scissors"),
+                    (_1, _2, acted, group) => {
+                        const gSet = new Set(acted.Appearance.map((a) => a.Asset.Group.Name));
+                        return groupMap[group.Name]?.some((g) => gSet.has(g));
+                    },
+                ],
+                MaxProgress: 50,
+                Target: /** @type {AssetGroupItemName[]} */ (Object.keys(groupMap)),
+                TargetSelf: true,
+            },
+            useImage: ["ItemHandheld", "Scissors"],
+            mode: "AnyOnSelf",
+            run: (player, sender, info) => {
+                // 遵守物品权限
+                if (!ServerChatRoomGetAllowItem(sender, player)) return;
 
-            // 使用动作拓展才会被剪衣服，可以只处理收到消息的情况
-            const groups = groupMap[info.ActivityGroup.Name];
-            if (groups) {
-                player.Appearance = player.Appearance.filter((i) => !groups.includes(i.Asset.Group.Name));
-                ChatRoomCharacterUpdate(player);
-            }
+                // 使用动作拓展才会被剪衣服，可以只处理收到消息的情况
+                const groups = groupMap[info.ActivityGroup.Name];
+                if (groups) {
+                    player.Appearance = player.Appearance.filter((i) => !groups.includes(i.Asset.Group.Name));
+                    ChatRoomCharacterUpdate(player);
+                }
+            },
         },
-    },
+    ],
     {
         CN: {
             ItemTorso: "上衣",
@@ -44,6 +64,9 @@ const activity = ActivityExt.fromTemplateActivity(
             ItemVulvaPiercings: "内裤",
             ItemFeet: "袜子",
             ItemBoots: "鞋子",
+            ItemHead: "帽子",
+            ItemMouth: "面具",
+            ItemArms: "手套",
         },
         EN: {
             ItemTorso: "Top",
@@ -52,6 +75,9 @@ const activity = ActivityExt.fromTemplateActivity(
             ItemVulvaPiercings: "Underwear",
             ItemFeet: "Socks",
             ItemBoots: "Shoes",
+            ItemHead: "Hat",
+            ItemMouth: "Mask",
+            ItemArms: "Gloves",
         },
         UA: {
             ItemTorso: "Верхній одяг",
@@ -60,6 +86,9 @@ const activity = ActivityExt.fromTemplateActivity(
             ItemVulvaPiercings: "Трусики",
             ItemFeet: "Шкарпетки",
             ItemBoots: "Взуття",
+            ItemHead: "Шапка",
+            ItemMouth: "Маска",
+            ItemArms: "Рукавички",
         },
     },
     {
