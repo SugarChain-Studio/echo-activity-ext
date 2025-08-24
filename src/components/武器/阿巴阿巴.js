bd97d15313ee0f853167d8cd81db58f6a53007a0
dynilath <dynilath@qq.com>
2025-08-24 22:54:23 +0800

    feat: 瞄准动作

--- src/components/武器/阿巴阿巴.js
+++ src/components/武器/阿巴阿巴.js
@@ -3,87 +3,102 @@ import { ActivityManager } from "../../activityForward";
 import { Prereqs } from "../../prereqs";
 import { ActivityExt } from "../../activityext";
 
-/** @type { CustomActivity } */
-const activity = ActivityExt.fromTemplateActivity(
-    {
-        activity: {
-            Name: "阿巴阿巴",
-            Prerequisite: ["UseArms", "UseHands", Prereqs.Acting.GroupIs("ItemHandheld", ["阿巴阿巴_Luzi"])],
-            MaxProgress: 50,
-            Target: [
-                "ItemBreast",
-                "ItemButt",
-                "ItemMouth",
-                "ItemTorso",
-                "ItemEars",
-                "ItemArms",
-                "ItemNeck",
-                "ItemHead",
-                "ItemHood",
-                "ItemNose",
-                "ItemPelvis",
-                "ItemHands",
-                "ItemLegs",
-                "ItemFeet",
-                "ItemBoots",
-            ],
-            TargetSelf: true,
-        },
-        mode: "AnyInvolved",
-        run: (player, _, { SourceCharacter, TargetCharacter }) =>
-            playItemAudio(
-                Path.resolve("audio/阿巴阿巴.mp3"),
-                SourceCharacter === player.MemberNumber || TargetCharacter === player.MemberNumber
-            ),
-        useImage: ["ItemHandheld", "阿巴阿巴_Luzi"],
+const regionTexts = {
+    CN: {
+        ItemBreast: "乳房",
+        ItemButt: "屁股",
+        ItemMouth: "脸",
+        ItemTorso: "腰",
+        ItemEars: "耳朵",
+        ItemArms: "手臂",
+        ItemNeck: "脖子",
+        ItemHood: "头",
+        ItemHead: "眉心",
+        ItemNose: "鼻子",
+        ItemPelvis: "肚子",
+        ItemHands: "手",
+        ItemLegs: "大腿",
+        ItemFeet: "小腿",
+        ItemBoots: "脚",
     },
-    {
-        CN: {
-            ItemBreast: "乳房",
-            ItemButt: "屁股",
-            ItemMouth: "脸",
-            ItemTorso: "腰",
-            ItemEars: "耳朵",
-            ItemArms: "手臂",
-            ItemNeck: "脖子",
-            ItemHood: "头",
-            ItemHead: "眉心",
-            ItemNose: "鼻子",
-            ItemPelvis: "肚子",
-            ItemHands: "手",
-            ItemLegs: "大腿",
-            ItemFeet: "小腿",
-            ItemBoots: "脚",
-        },
-        EN: {
-            ItemBreast: "Breast",
-            ItemButt: "Butt",
-            ItemMouth: "Mouth",
-            ItemTorso: "Torso",
-            ItemEars: "Ears",
-            ItemArms: "Arms",
-            ItemNeck: "Neck",
-            ItemHood: "Hood",
-            ItemHead: "Head",
-            ItemNose: "Nose",
-            ItemPelvis: "Abdomen",
-            ItemHands: "Hands",
-            ItemLegs: "Thighs",
-            ItemFeet: "Calves",
-            ItemBoots: "Feet",
-        },
+    EN: {
+        ItemBreast: "Breast",
+        ItemButt: "Butt",
+        ItemMouth: "Mouth",
+        ItemTorso: "Torso",
+        ItemEars: "Ears",
+        ItemArms: "Arms",
+        ItemNeck: "Neck",
+        ItemHood: "Hood",
+        ItemHead: "Head",
+        ItemNose: "Nose",
+        ItemPelvis: "Abdomen",
+        ItemHands: "Hands",
+        ItemLegs: "Thighs",
+        ItemFeet: "Calves",
+        ItemBoots: "Feet",
     },
-    {
-        label: {
-            CN: "射击$group",
-            EN: "Shoot $group",
+};
+
+const regions = /** @type {AssetGroupItemName[]} */ (Object.keys(regionTexts.CN));
+
+/** @type { CustomActivity[] } */
+const activity = [
+    ActivityExt.fromTemplateActivity(
+        {
+            activity: {
+                Name: "阿巴阿巴",
+                Prerequisite: ["UseArms", "UseHands", Prereqs.Acting.GroupIs("ItemHandheld", ["阿巴阿巴_Luzi"])],
+                MaxProgress: 50,
+                Target: regions,
+                TargetSelf: true,
+            },
+            mode: "AnyInvolved",
+            run: (player, _, { SourceCharacter, TargetCharacter }) =>
+                playItemAudio(
+                    Path.resolve("audio/阿巴阿巴.mp3"),
+                    SourceCharacter === player.MemberNumber || TargetCharacter === player.MemberNumber
+                ),
+            useImage: ["ItemHandheld", "阿巴阿巴_Luzi"],
+            item: (actor) => InventoryGet(actor, "ItemHandheld"),
         },
-        dialog: {
-            CN: "SourceCharacter举起枪瞄准, 水弹直直击中了DestinationCharacter$group。",
-            EN: "SourceCharacter raised the gun and aimed, the water bullet hit DestinationCharacter $group directly.",
+        regionTexts,
+        {
+            label: {
+                CN: "射击$group",
+                EN: "Shoot $group",
+            },
+            dialog: {
+                CN: "SourceCharacter举起ActivityAsset瞄准, 水弹直直击中了DestinationCharacter$group。",
+                EN: "SourceCharacter raised the ActivityAsset and aimed, the water bullet hit DestinationCharacter $group directly.",
+            },
+        }
+    ),
+    ActivityExt.fromTemplateActivity(
+        {
+            activity: {
+                Name: "阿巴阿巴_瞄准",
+                Prerequisite: ["UseArms", "UseHands", Prereqs.Acting.GroupIs("ItemHandheld", ["阿巴阿巴_Luzi"])],
+                MaxProgress: 50,
+                Target: regions,
+            },
+            mode: "AnyInvolved",
+            useImage: ["ItemHandheld", "阿巴阿巴_Luzi"],
+            item: (actor) => InventoryGet(actor, "ItemHandheld"),
         },
-    }
-);
+        regionTexts,
+        {
+            label: {
+                CN: "瞄准$group",
+                EN: "Aim at $group",
+            },
+            dialog: {
+                CN: "SourceCharacter举起ActivityAsset，瞄准了DestinationCharacter$group。",
+                EN: "SourceCharacter raised the ActivityAsset and aimed at DestinationCharacter $group.",
+            },
+        }
+    ),
+];
 
 export default function () {
     ActivityManager.addCustomActivity(activity);
