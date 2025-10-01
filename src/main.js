06f9856f236da489dc475439ea94a6558f6acb32
dynilath <dynilath@qq.com>
2025-10-01 20:12:46 +0800

    chore: 更新依赖，调整相关代码，增加更新依赖脚本

--- src/main.js
+++ src/main.js
@@ -6,12 +6,14 @@ import { ChatRoomOrder } from "@mod-utils/ChatRoomOrder";
 import { setup } from "./components";
 import { once } from "@sugarch/bc-mod-utility";
 import { Logger } from "@mod-utils/log";
+import bcModSdk from "bondage-club-mod-sdk";
 
 HookManager.setLogger(Logger);
 ActivityManager.setLogger(Logger);
 
 once(ModInfo.name, () => {
-    HookManager.init(ModInfo);
+    const mod = bcModSdk.registerMod(ModInfo);
+    HookManager.initWithMod(mod);
     ChatRoomOrder.setup();
     ActivityManager.init();
     setup();
